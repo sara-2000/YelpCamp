5354544d9d52236f9e2f84e1bffc063980768cd9
sara-2000 <saradak605@gmail.com>
2020-04-30 17:01:06 +0530

    add database

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var campgroundRoutes = require("./routes/campgrounds"),
 	commentRoutes    = require("./routes/comments"),
 	indexRoutes		 = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp" , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect("mongodb+srv://Mongod:Sara%402003@cluster0-rrz9x.mongodb.net/test?retryWrites=true&w=majority" , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname + "/public"));
